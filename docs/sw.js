9ddaf7ef9232f16e333edc8ad38d9f03e12b6d70
lbseven7 <lbseven7@users.noreply.github.com>
2026-08-03 11:06:39 +0000

    deploy: 0f6a9f6f635c2c52628608c9fc0a6a471ea6097a

--- docs/sw.js
+++ docs/sw.js
@@ -1,10 +1,9 @@
-const CACHE = 'takeum-v28';
+const CACHE = 'takeum-v22';
 
 const ASSETS = [
   './',
   './index.html',
   './db.js',
-  './pro.js',
   './ia.js',
   './fonts.css',
   './fonts/archivo-500.woff2',
@@ -22,9 +21,7 @@ const ASSETS = [
   './image/prod-6.jpg',
   './image/prod-7.jpg',
   './newsletter-obrigado.html',
-  './obrigado-pro.html',
   './guia.html',
-  './vendas.html',
   './blog/mercado-criadores-de-video-solo.html',
   './guia-primeiro-video.html',
   './guia-microfone.html',
